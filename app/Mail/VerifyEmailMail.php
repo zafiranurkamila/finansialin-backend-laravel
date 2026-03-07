c8bb94f1b5538e95f495f06ac411afd52fc86993
Zafira Nur Kamila <zafira@gmail.com>
2026-03-07 12:52:28 +0700

    backend laravel

--- app/Mail/VerifyEmailMail.php
+++ app/Mail/VerifyEmailMail.php
@@ -1,35 +1,4 @@
-c:\Users\zafir\Downloads\Semester 5\ippl\project\finansialin-backend-laravel>git commit -m "backend laravel"
-On branch master
-
-Initial commit
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)      
-        .editorconfig
-        .env.example
-        .gitattributes
-        .gitignore
-        README.md
-        app/
-        artisan
-        bootstrap/
-        composer.json
-        composer.lock
-        config/
-        database/
-        package.json
-        phpunit.xml
-        public/
-        resources/
-        routes/
-        storage/
-        tests/
-        vite.config.js
-
-nothing added to commit but untracked files present (use "git add" to 
-track)
-
-c:\Users\zafir\Downloads\Semester 5\ippl\project\finansialin-backend-laravel><?php
+<?php
 
 namespace App\Mail;
 
